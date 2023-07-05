124942773730f003d65f3576de70d9305a6043b8
Cory Gold <goldcorum@gmail.com>
2023-07-05 10:39:34 -0400

    Setup demo file

---
+++ demo.js
@@ -0,0 +1,52 @@
+/* **** Use Comparison Operators to compare values  **** */
+
+//Comparison Operator 1: Greater Than
+
+//Comparison Operator 2: Less Than
+
+//Comparison Operator 3: Loosely Equals
+
+//Comparison Operator 4: Strictly Equals
+
+//Comparison Operator 5: Greater than or Loosely Equal To
+
+//Comparison Operator 6: Less than or Loosely Equal To
+
+/* **** Use Logical Operators to Evaluate Boolean Expressions  **** */
+
+//Logical Operator 1: The AND Operator (&&)
+
+//Logical Operator 2: The OR Operator (||)
+
+//Logical Operator 3: The NOT Operator (!)
+
+/* **** Apply Control Flow using If, Else If, and Else Statements  **** */
+
+/*Control Flow with IF Statements*/
+
+//If Statement 1: Using Variables, Numbers, and Comparison Operators
+
+//If Statement 2: Using Variables, Strings, and Includes String Method
+
+//If Statement 3: Using the Boolean Value true
+
+//If Statement 4: Using Logical Operator AND (&&)
+
+//If Statement 5: Using Logical Operator OR (||)
+
+//If Statement 6: Using Logical Operator NOT (!)
+
+/*Control Flow with IF and Else Statements*/
+
+//If and Else Statement 1 Using Variables, Numbers, and Comparison Operators
+
+//If and Else Statement 2: Using Variables, Strings, and Includes String Method
+
+//If and Else Statement 3: Using the Boolean Value true
+
+/*Control Flow with IF, Else IF, Else Statements*/
+
+//If Else IF Else Statement : Using Logical Operator AND (&&) and OR (||)
+
+//If, Else IF, and Else Statement 2: Using Variables, Numbers, and Comparison Operators
+
